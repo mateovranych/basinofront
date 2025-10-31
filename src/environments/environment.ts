0bc8a481c1a1ab1bbbaf4fd3b1f90cf6baffc758
mateovranych <mateovranych@gmail.com>
2025-10-31 00:06:49 -0300

    agregue la /api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     
   production: true,
   // apiUrl: 'https://localhost:7004/api'    
-  apiUrl: 'https://bassino-api-f8ezgzg0gthfhmar.brazilsouth-01.azurewebsites.net'
+  apiUrl: 'https://bassino-api-f8ezgzg0gthfhmar.brazilsouth-01.azurewebsites.net/api'
 
 
 };
